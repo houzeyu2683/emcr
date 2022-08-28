77e502edfacc69122c07af12bc92f70f9a88f80b
Greg <houzeyu2683@gmail.com>
2022-08-28 14:01:55 +0800

    'local'

--- prototype.py
+++ prototype.py
@@ -5,55 +5,56 @@ import pandas
 import os
 
 ##
+##  Read sheets.
+resource = './resource/kaggle/'
+path = [
+    os.path.join(resource, 'train.csv'), 
+    os.path.join(resource, 'test.csv'), 
+    os.path.join(resource, 'sample_submission.csv'),
+    os.path.join(resource, 'historical_transactions.csv'),
+    os.path.join(resource, 'new_merchant_transactions.csv'),
+    os.path.join(resource, 'merchants.csv')
+]
+sheet = {os.path.basename(p).split('.')[0]: pandas.read_csv(p, dtype=str) for p in path}
+sheet.keys()
+
+##
 ##  Storage folder.
-storage = './resource/prototype/'
+storage = './resource/kaggle/prototype/'
 os.makedirs(os.path.dirname(storage), exist_ok=True)
 
 ##
-##  Read sheets.
-path = [
-    './resource/kaggle/train.csv', 
-    './resource/kaggle/test.csv', 
-    './resource/kaggle/sample_submission.csv',
-    './resource/kaggle/historical_transactions.csv',
-    './resource/kaggle/new_merchant_transactions.csv',
-    './resource/kaggle/merchants.csv'
-]
-sheet = [(os.path.basename(p), pandas.read_csv(p, dtype=str)) for p in path]
-
-##
-##  Combine the train and test together.
-table = {'train': sheet[0][1], 'test': sheet[1][1]}
+##  Combine `train` and `test` to `card`.
+table = {'train': sheet.get('train'), 'test': sheet.get('test')}
+table['train']['mode'] = 0
+table['test']['mode'] = 1
 table['test']['target'] = None
-table['index'] = pandas.concat([table['train'], table['test']], axis=0)
-table['index'].to_csv(os.path.join(storage, 'index.csv'), index=False)
+table['card'] = pandas.concat([table['train'], table['test']], axis=0)
+table['card'].to_csv(os.path.join(storage, 'card.csv'), index=False)
 del table
 
 ##
-##  Combine the historical transaction and merchant together.
+##  Combine `historical_transactions`, `new_merchant_transactions` and `merchant`.
 table = {
-    'transaction-0': sheet[3][1], 
-    'transaction-1': sheet[4][1], 
-    'merchant':sheet[5][1]
+    'historical_transactions': sheet.get("historical_transactions"), 
+    'new_merchant_transactions': sheet.get("new_merchant_transactions"), 
+    'merchant':sheet.get("merchants")
 }
-##  Two transaction table merge and remove repeat row if need.
-table['transaction'] = pandas.concat([table['transaction-0'], table['transaction-1']], axis=0)
+##  Merge `historical_transactions` and `new_merchant_transactions` to `transaction`.
+##  Then remove repeat row.
+table['transaction'] = pandas.concat([table['historical_transactions'], table['new_merchant_transactions']], axis=0)
 table['transaction'] = table['transaction'].drop_duplicates()
-##  The merchant table and transaction table have some same column,
-##  but the `merchant_id` key for merge.
-##  Then remove the repeat column in the merchant table.
+##  Find `merchant` and `transaction` have some same columns,
+##  Remove some columns of `merchant` if `transaction` already exist.
+##  Keep `merchant_id` exist and merge later.
 key = 'merchant_id'
 repeat = list(set(table['transaction'].keys()) & set(table['merchant'].keys()))
 repeat.remove(key)
 table['merchant'] = table['merchant'].drop(repeat, axis=1)
-##  And the merchant table have some problem,
-##  the 'merchant_id' is not unique, 
+##  Found 'merchant_id' of `merchant` is not unique,
 ##  then drop and keep the last in the table.
 table['merchant'] = table['merchant'].drop_duplicates(subset=[key], keep='last')
-##  Merge the transaction table and merchant table together, join with transaction table.
-##  So if some 'merchant_id' exist in the transaction table but not in the merchant table,
-##  that mean value of column from the merchant table is missing value, we will need to handle it.
-##  We call the big table denote by the history table.
+##  Insert `merchant` to `transaction`, denote `history`.
 table['history'] = pandas.merge(table['transaction'], table['merchant'], how='left', on=key)
 table['history'].to_csv(os.path.join(storage, 'history.csv'), index=False)
-
+del table
--- scatter.py
+++ scatter.py
@@ -4,10 +4,11 @@
 import pandas
 import numpy
 import os
+import sklearn.preprocessing
 
 ##
 ##  Read sheets.
-resource = "./resource/kaggle/prototype/"
+resource = "./resource/kaggle/prototype/clean/"
 path = [
     os.path.join(resource, 'card.csv'), 
     os.path.join(resource, 'history.csv')
@@ -16,31 +17,30 @@ sheet = {os.path.basename(p).split(".")[0]: pandas.read_csv(p, dtype=str) for p 
 
 ##
 ##  Storage folder.
-storage = './resource/kaggle/prototype/clean/'
+storage = './resource/kaggle/prototype/clean/scatter/'
 os.makedirs(os.path.dirname(storage), exist_ok=True)
 
 ##
 ##  Handle `card`.
 key = 'card'
 table = sheet.get(key)
-table.head()
-
-
-
-
-table['first_active_month'] = table['first_active_month'].astype(str)
-
-table['first_active_month'].isna().sum()
-table['card_id'] = table['card_id'].astype(str)
-table['feature_1'] = table['feature_1'].astype(int)
-
-
 table['feature_1'] = table['feature_1'].astype(float)
 table['feature_2'] = table['feature_2'].astype(float)
 table['feature_3'] = table['feature_3'].astype(float)
 table['target']    = table['target'].astype(float)
 table['mode']      = table['mode'].astype(float)
-table.loc[table['first_active_month'].isna()] = '2017-09'
+##  Generate variable.
+table['first_active_year_unit']  = [i.split('-')[0] for i in table['first_active_month']]
+table['first_active_month_unit'] = [i.split('-')[1] for i in table['first_active_month']]
+##  Convert to code.
+column = ['first_active_month', 'first_active_year_unit', 'first_active_month_unit']
+for c in column:
+    
+    engine = sklearn.preprocessing.LabelEncoder()
+    _ = engine.fit(table[c])
+    table[c] = engine.transform(table[c])
+    continue
+
 table.to_csv(os.path.join(storage, 'card.csv'), index=False)
 del table
 
@@ -68,21 +68,22 @@ table['active_months_lag6']   = table['active_months_lag6'].astype(float)
 table['avg_sales_lag12']      = table['avg_sales_lag12'].astype(float)
 table['avg_purchases_lag12']  = table['avg_purchases_lag12'].astype(float)
 table['active_months_lag12']  = table['active_months_lag12'].astype(float)
-##  Before fill missing value of number, limit the inf to a higher value.
-table.loc[table['avg_purchases_lag3']==numpy.inf, 'avg_purchases_lag3'] = 10000 + 61851.33333333
-table.loc[table['avg_purchases_lag6']==numpy.inf, 'avg_purchases_lag6'] = 10000 + 56077.5
-table.loc[table['avg_purchases_lag12']==numpy.inf, 'avg_purchases_lag12'] = 10000 + 50215.55555556
-for i in table:
-
-    if(table[i].dtype == float): table[i] = table[i] - table[i].min()
-    continue
-
-for i in table:
-
-    if(table[i].dtype == float): table[i] = table[i].fillna(-1.0)
-    if(table[i].dtype != float): table[i] = table[i].fillna('<NA>')
+##  Generate variable.
+table['purchase_date_month'] = [i.split(' ')[0] for i in table['purchase_date']]
+##  Convert to code.
+column = [
+    'authorized_flag', 'category_1', 'category_3', 'merchant_id',
+    'most_recent_sales_range', 'most_recent_purchases_range', 'category_4'
+]
+for c in column:
+    
+    engine = sklearn.preprocessing.LabelEncoder()
+    _ = engine.fit(table[c])
+    table[c] = engine.transform(table[c])
     continue
 
 table.to_csv(os.path.join(storage, 'history.csv'), index=False)
 del table
+
+
 
